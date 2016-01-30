6dfdbf1e9003f570be6ea6ac4290c386a3726047
PetarDDobrev <petarddobrev@abv.bg>
2016-01-30 14:19:49 +0200

    Player Health and Ale added

--- Assets/Completed/Scripts/Player.cs
+++ Assets/Completed/Scripts/Player.cs
@@ -11,8 +11,11 @@ namespace Completed
 		public int pointsPerFood = 10;				//Number of points to add to player food points when picking up a food object.
 		public int pointsPerSoda = 20;				//Number of points to add to player food points when picking up a soda object.
 		public int wallDamage = 1;                  //How much damage a player does to a wall when chopping it.
-        public int enemyDamage = 1;                 //How much damge a player does to a enemy.
-        public Text foodText;						//UI Text to display current player food total.
+        public int m_Damage = 1;                 //How much damge a player does to a enemy.
+		public int m_HealthMax = 0;
+		public int m_AleMax = 0;
+		public Text m_AleText;                      //UI Text to display current player food total.
+		public Text m_HealthText;                   //UI Text to display current player food total.
 		public AudioClip moveSound1;				//1 of 2 Audio clips to play when player moves.
 		public AudioClip moveSound2;				//2 of 2 Audio clips to play when player moves.
 		public AudioClip eatSound1;					//1 of 2 Audio clips to play when player collects a food object.
@@ -22,24 +25,25 @@ namespace Completed
 		public AudioClip gameOverSound;				//Audio clip to play when player dies.
 		
 		private Animator animator;					//Used to store a reference to the Player's animator component.
-		private int food;							//Used to store player food points total during level.
+		private int m_AleCurrent = 0;      
+		private int m_HealthCurrent = 0;
 		private Vector2 touchOrigin = -Vector2.one;	//Used to store location of screen touch origin for mobile controls.
 		
 		
 		//Start overrides the Start function of MovingObject
 		protected override void Start ()
 		{
-			//Get a component reference to the Player's animator component
-			animator = GetComponent<Animator>();
-			
-			//Get the current food point total stored in GameManager.instance between levels.
-			food = GameManager.instance.playerFoodPoints;
-			
-			//Set the foodText to reflect the current player food total.
-			foodText.text = "Food: " + food;
-			
 			//Call the Start function of the MovingObject base class.
 			base.Start ();
+
+			//Get a component reference to the Player's animator component
+			animator = GetComponent<Animator>();
+
+			//Get the current food point total stored in GameManager.instance between levels.
+			m_AleCurrent = m_AleMax;
+			ChangeAle( 0 );
+			m_HealthCurrent = m_HealthMax;
+			SyncHealthText();
 		}
 		
 		
@@ -47,9 +51,8 @@ namespace Completed
 		private void OnDisable ()
 		{
 			//When Player object is disabled, store the current local food total in the GameManager so it can be re-loaded in next level.
-			GameManager.instance.playerFoodPoints = food;
-		}
-		
+			//GameManager.instance.playerFoodPoints = food;
+		}		
 		
 		private void Update ()
 		{
@@ -130,11 +133,7 @@ namespace Completed
 		protected override void AttemptMove <T> (int xDir, int yDir)
 		{
 			//Every time player moves, subtract from food points total.
-			food--;
-			
-			//Update food text display to reflect current score.
-			foodText.text = "Food: " + food;
-			
+			ChangeAle(-1);
 			//Call the AttemptMove method of the base class, passing in the component T (in this case Wall) and x and y direction to move.
 			base.AttemptMove <T> (xDir, yDir);
 			
@@ -173,11 +172,8 @@ namespace Completed
             {
                 hitEnemy = component as Enemy;
                 //Call the DamageEnemy function from the Enemy we are hitting 
-                hitEnemy.DamageEnemy(enemyDamage);
+                hitEnemy.DamageEnemy(m_Damage);
             }
-         
-            
-			
          
 			//Set the attack trigger of the player's animation controller in order to play the player's attack animation.
 			animator.SetTrigger ("playerChop");
@@ -201,11 +197,8 @@ namespace Completed
             //Check if the tag of the trigger collided with is Food.
             else if (other.tag == "Food")
             {
-                //Add pointsPerFood to the players current food total.
-                food += pointsPerFood;
-
-                //Update foodText to represent current total and notify player that they gained points
-                foodText.text = "+" + pointsPerFood + " Food: " + food;
+				ChangeAle(pointsPerFood);
+				
 
                 //Call the RandomizeSfx function of SoundManager and pass in two eating sounds to choose between to play the eating sound effect.
                 SoundManager.instance.RandomizeSfx(eatSound1, eatSound2);
@@ -217,11 +210,7 @@ namespace Completed
             //Check if the tag of the trigger collided with is Soda.
             else if (other.tag == "Soda")
             {
-                //Add pointsPerSoda to players food points total
-                food += pointsPerSoda;
-
-                //Update foodText to represent current total and notify player that they gained points
-                foodText.text = "+" + pointsPerSoda + " Food: " + food;
+				ChangeAle(pointsPerSoda);
 
                 //Call the RandomizeSfx function of SoundManager and pass in two drinking sounds to choose between to play the drinking sound effect.
                 SoundManager.instance.RandomizeSfx(drinkSound1, drinkSound2);
@@ -243,27 +232,24 @@ namespace Completed
 		
 		//LoseFood is called when an enemy attacks the player.
 		//It takes a parameter loss which specifies how many points to lose.
-		public void LoseFood (int loss)
+		public void TakeDamage(int dmg)
 		{
 			//Set the trigger for the player animator to transition to the playerHit animation.
 			animator.SetTrigger ("playerHit");
-			
-			//Subtract lost food points from the players total.
-			food -= loss;
-			
-			//Update the food display with the new total.
-			foodText.text = "-"+ loss + " Food: " + food;
-			
-			//Check to see if game has ended.
-			CheckIfGameOver ();
+			m_HealthCurrent -= dmg;
+			SyncHealthText();
 		}
-		
-		
+
+		private void SyncHealthText()
+		{
+			m_HealthText.text = "Health:" + m_HealthCurrent;
+		}
+
 		//CheckIfGameOver checks if the player is out of food points and if so, ends the game.
 		private void CheckIfGameOver ()
 		{
 			//Check if food point total is less than or equal to zero.
-			if (food <= 0) 
+			if (m_HealthCurrent <= 0) 
 			{
 				//Call the PlaySingle function of SoundManager and pass it the gameOverSound as the audio clip to play.
 				SoundManager.instance.PlaySingle (gameOverSound);
@@ -272,8 +258,14 @@ namespace Completed
 				SoundManager.instance.musicSource.Stop();
 				
 				//Call the GameOver function of GameManager.
-				GameManager.instance.GameOver ();
+				GameManager.instance.GameOver();
 			}
+		}
+
+		private void ChangeAle( int change )
+		{
+			m_AleCurrent += change;
+			m_AleText.text = "Ale:" + m_AleCurrent; 
 		}
 	}
 }
